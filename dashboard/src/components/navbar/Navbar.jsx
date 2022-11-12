193d10366c07f08a1ef10a8b53405338f3017b93
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-11-12 11:22:25 +0100

    logo

---
+++ dashboard/src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css";
+import thofLogo from "..//../assets/GoldGradientmedium.png"
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <img src={thofLogo} alt="thof-logo" />
+      div.
+    </div>
+  )
+}
