d95fed7424d88c9dad4e8c1968e9a3b2f45a2e1f
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-11-13 21:11:21 +0100

    sync

--- dashboard/src/components/chart/Chart.jsx
+++ dashboard/src/components/chart/Chart.jsx
@@ -1,13 +1,10 @@
-import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 import "./chart.css"
 export default function Chart() {
   return (
     <div className="chart">
       <h3 className="chartTitle">Sales Insights</h3>
-      <ResponsiveContainer width="100%" >
-
-      </ResponsiveContainer>
+      
     </div>
   )
 }
