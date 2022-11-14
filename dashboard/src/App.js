823129aa64980a1f4478ef436c7de681e9684a05
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-11-15 00:03:16 +0100

    routing

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,16 +2,23 @@ import "./App.css"
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import Products from "./pages/products/Products";
 
 function App() {
   return (
-    <div >
+    <Router >
       <Navbar/>
       <div className="container">
         <Sidebar/>
-        <Home/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route path="/users" elment={<UserList />}></Route>
+          <Route path="/products" elment={<Products />}></Route>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ dashboard/src/pages/products/Products.js
@@ -0,0 +1,7 @@
+import "./products.css"
+
+export default function Products() {
+  return (
+    <div className="products">Products</div>
+  )
+}
