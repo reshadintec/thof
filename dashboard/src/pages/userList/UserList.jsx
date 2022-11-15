11ee54e041921bdc23b2dd33365d246e65b666cb
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-11-15 21:55:07 +0100

    users page done

--- dashboard/src/pages/userList/UserList.jsx
+++ dashboard/src/pages/userList/UserList.jsx
@@ -1,9 +1,8 @@
 import './userList.css'
 import { DataGrid } from '@mui/x-data-grid';
 import {DeleteOutlineOutlined} from '@mui/icons-material';
-import { border } from '@mui/system';
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'id', headerName: 'ID', width: 60 },
   { field: 'user', headerName: 'User name ', width: 200, renderCell:(params)=>{
     return (
       <div className='userListUser'>
@@ -14,7 +13,7 @@ const columns = [
   } },
   { field: 'email', headerName: 'Email', width: 150 },
   { field: 'status', headerName: 'Status', width: 150 },
-  { field: 'transaction', headerName: 'Transaction Volume', width: 160},
+  { field: 'transaction', headerName: 'Transaction Volume', width: 150},
   { field: 'action', headerName: 'Action ', width: 150,
     renderCell:(params) =>{
       return(
@@ -101,8 +100,8 @@ const rows = [
 export default function UserList() {
   return (
     <div className="userList">
-      <div style={{ height: 700, width: '100%' }}>
-        <DataGrid sx={{ color: '#fff', border:'solid 1px #e3cb99 '}}
+      <div style={{ height: 550, width: '100%' }}>
+        <DataGrid sx={{ color: '#fff', border:'solid 1px #e3cb99 '}} className="dataGridCustom"
           rows={rows} disableSelectionOnClick
           columns={columns}
           pageSize={8}
