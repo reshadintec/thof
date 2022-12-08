bbe8107b0d1ffa14ba89e6662c9ccc84850c13b3
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-12-08 20:09:19 +0100

    footer bijna

--- uidesign/src/components/Footer.jsx
+++ uidesign/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import LogoSrc from "../assets/GoldGradientmedium.png";
-import {FacebookOutlined, Instagram, LinkedIn, Twitter, YouTube} from '@mui/icons-material';
+import {FacebookOutlined, Instagram, LinkedIn, MailOutline, MapOutlined, Phone, Room, Twitter, YouTube} from '@mui/icons-material';
 const Container = styled.div`
   height: 40vh;
-  background-color: #000;
+  background-color: #0b0b0b;
   display: flex;
   justify-content: space-between;
-  padding: 60px 3%;
+  padding: 60px 3% 20px;
 `;
 const Left = styled.div`
  flex: 1;
@@ -26,19 +26,18 @@ const Desc = styled.p`
   line-height: 1.4;
   letter-spacing: 1.8px;
   width: 80%;
-  margin-bottom: 20px;
+  margin-bottom: 30px;
 `;
 
 const SocialIcons = styled.div`
   display: flex;
-  justify-content: flex-start;
 `;
 
 const Social = styled.div`
   width: 40px;
   height: 40px;
   background-color: #bf963f;
-  margin: 0 10px;
+  margin-right: 20px;
   color:white;
   display: flex;
   justify-content: center;
@@ -50,15 +49,49 @@ const Social = styled.div`
   }
   `;
 
-
-
 const Center = styled.div`
  flex: 1;
+ display: flex;
+ flex-direction: column;
+ flex-wrap: wrap;
+ align-content: center;
 `;
+
+const Title= styled.h4`
+  color: white;
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 8%;
+`;
+
+const Link = styled.a`
+  color: #e3cb99;
+  opacity: 0.6;
+  font-size: 18px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  transition: all 0.5s ease;
+  &:hover{
+    opacity: 1;
+  }
+`;
+
 const Right = styled.div`
  flex: 1;
 `;
 
+const ContactContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const ContactDesc = styled.p`
+  color:white;
+  font-weight: 200;
+  font-size: 15px;
+  margin-left: 10px;
+`;
 
 
 export const Footer = () => (
@@ -86,7 +119,35 @@ export const Footer = () => (
         </Social>
       </SocialIcons>
     </Left>
-    <Center>Center</Center>
-    <Right>Right</Right>
+    <Center>
+      <Title>
+        Useful Links
+      </Title>
+      
+      <Link>My Account</Link>
+      <Link>Orders</Link>
+      <Link>Perfumes For Her</Link>
+      <Link>Perfumes For Him</Link>
+      <Link>Privacy Policy</Link>
+      <Link>Refund & Return</Link>
+      <Link>T&Cs</Link>
+    </Center>
+    <Right>
+      <Title>
+        Contact Details
+      </Title>
+      <ContactContainer>
+        <Room style={{fontSize:"1.8rem", color:"white"}}/>
+        <ContactDesc>Some Address in my Town 3500</ContactDesc>
+      </ContactContainer>
+      <ContactContainer>
+        <Phone style={{fontSize:"1.8rem", color:"white"}}/>
+        <ContactDesc>+32 492 92 92 92</ContactDesc>
+      </ContactContainer>
+      <ContactContainer>
+        <MailOutline style={{fontSize:"1.8rem", color:"white"}}/>
+        <ContactDesc>mail@thehouseoffragrance.com</ContactDesc>
+      </ContactContainer>
+    </Right>
   </Container>
 )
