63bdf086513e329bf9ccab645c51dfd00e2cd4f1
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-12-09 19:45:30 +0100

    footer update

--- uidesign/src/components/Footer.jsx
+++ uidesign/src/components/Footer.jsx
@@ -120,7 +120,7 @@ const ContactDesc = styled.p`
 `;
 
 const Payment = styled.img`
-  max-width: 150px;
+  max-width: 200px;
   @media(min-width: 1442px){
     margin-top: 10px;
     max-width: 260px;
