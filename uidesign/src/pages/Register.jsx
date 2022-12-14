332068248ccf7c8209aad349f998ad246aa9daf2
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-12-14 22:45:17 +0100

    sync

--- uidesign/src/pages/Register.jsx
+++ uidesign/src/pages/Register.jsx
@@ -4,11 +4,24 @@ const Container = styled.div`
   
 `;
 
+const RegisterContainer = styled.div`
+  width: 80%;
+  height: 60vh;
+  background: rgb(63,13,18);
+  background: radial-gradient(circle, rgba(63,13,18,1) 0%, rgba(23,5,5,1) 50%);
+  margin: 20px auto;
+  color: #e3cb99;
+  padding: 20px;
+`;
+
 
 export const Register = () => {
   return (
     <Container>
-      Register
+      
+      <RegisterContainer>
+        Register Container
+      </RegisterContainer>
     </Container>
   )
 }
