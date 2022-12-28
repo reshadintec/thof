51102f6f350e7293db8f77e37db5eeac7835cb3a
reshadintec <105705121+reshadintec@users.noreply.github.com>
2022-12-28 22:43:01 +0100

    left section cart page

--- uidesign/src/pages/Cart.jsx
+++ uidesign/src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Close, Remove } from "@mui/icons-material";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import { Footer } from "../components/Footer";
@@ -29,8 +30,9 @@ const TopButton = styled.button`
   cursor: pointer;
   border: ${props=>props.type==="filled" && "none"};
   background-color: ${props=>props.type === "filled" && "#bf963f"};
-  padding: ${props=> props.type === "filled" && "10px 20px"};
+  padding: ${props=> props.type === "filled" && "14px 20px"};
   color: ${props=>props.type === "filled" && "#fffbd2"};
+  background-color: ${props=>props.type === "simple" && "transparent"};
   transition: all 1s ease;
   &:hover{
     background-color: ${props=>props.type === "filled" && "#aa771c"};
@@ -41,43 +43,82 @@ const Bottom = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding-top: 20px;
+  padding-top: 60px;
 `;
 const Info = styled.div`
   flex: 3;
+  display: flex;
+  flex-direction: column;
+  margin-right: 20px;
 `;
 
+const Titles = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 40px;
+`;
+
+const SingleTitle = styled.span`
+  font-size: 12px;
+  color: gray;
+  flex-direction: column;
+  text-align:center;
+  flex: 1;
+  &:first-child{
+    flex:2;
+    text-align: left;
+  }
+`;
 const  Product = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
-const  ProductDetail = styled.div`
+
+const ProductItem = styled.div`
+  flex:1;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  flex-direction: column;
+  align-content: center;
+  &:first-child{
+    flex:2;
+  };
+  &:first-child{
+   flex-direction: row;
+  }
+`;
+
+const  Image = styled.img`
+  width: 60px;
+  margin-right: 20px;
+`;
+
+const ProductTitle = styled.h4`
+  font-weight: 400;
+`;
+
+const ProductText = styled.h4`
+  font-weight: 300;
+  margin: ${props=> props.kind === "qty" && "0 10px"};
+`;
+
+const PriceContainer = styled.div`
   display: flex;
   align-items: center;
 `;
-const  Image = styled.img`
-  width: 300px;
-  margin-right: 20px;
-`;
-const  Details = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-const  ProductName = styled.div``;
-const  ProductSize = styled.div``;
-const  ProductGender = styled.div``;
-const  ProductStrength = styled.div``;
-const  PriceDetail = styled.div`
- 
-`; 
-
-
-
 const Summary = styled.div`
   flex:1 ; 
 `;
+
+const Line = styled.hr`
+  color: gray;
+  width: 100%;
+  margin: 20px 0;
+  border: none;
+  border-bottom: 2px solid lightgray;
+`
 
 export const Cart = () => {
   return (
@@ -87,37 +128,53 @@ export const Cart = () => {
       <CartContainer>
        <Title>Shopping Cart</Title>
        <Top>
-        <TopButton>Continue Shopping</TopButton>
+        <TopButton type="simple">Continue Shopping</TopButton>
         <TopButton type="filled">Checkout Now</TopButton>
        </Top>  
        <Bottom>
         <Info>
+          <Titles>
+            <SingleTitle>ITEM</SingleTitle>
+            <SingleTitle>PRICE</SingleTitle>
+            <SingleTitle>QUANTITY</SingleTitle>
+            <SingleTitle>REMOVE</SingleTitle>
+          </Titles>
           <Product>
-            <ProductDetail>
+             <ProductItem>
               <Image src="https://uz.thehouseoffragrance.com/wp-content/uploads/sites/2/2022/10/Masque-Milano-Tango.webp"/>
-              <Details>
-                <ProductName>
-                  <b>Product: </b>
-                  Masque Milano Tango
-                </ProductName>
-                <ProductSize>
-                  <b>Size: </b>
-                  35 ml
-                </ProductSize>
-                <ProductGender>
-                  <b>Gender: </b>
-                  Unisex
-                </ProductGender>
-                <ProductStrength>
-                  <b>Strength: </b>
-                  Eau De Parfum
-                </ProductStrength>
-              </Details>
-            </ProductDetail>
-            <PriceDetail>
-              Price
-            </PriceDetail>
+              <ProductTitle>Masque Milano Tango</ProductTitle>
+             </ProductItem>
+             <ProductItem>
+              <ProductText>$ 132</ProductText>
+             </ProductItem>
+             <ProductItem>
+              <PriceContainer>
+                <Remove sx={{fontSize: "16px"}}/>
+                <ProductText kind="qty">1</ProductText>
+                <Add sx={{fontSize: "16px"}}/>
+              </PriceContainer>
+             </ProductItem>
+             <ProductItem><Close sx={{fontSize: "20px"}}/></ProductItem>
           </Product> 
+          <Line/>
+          <Product>
+             <ProductItem>
+              <Image src="https://uz.thehouseoffragrance.com/wp-content/uploads/sites/2/2022/10/Masque-Milano-Tango.webp"/>
+              <ProductTitle>Masque Milano Tango</ProductTitle>
+             </ProductItem>
+             <ProductItem>
+              <ProductText>$ 132</ProductText>
+             </ProductItem>
+             <ProductItem>
+              <PriceContainer>
+                <Remove sx={{fontSize: "16px"}}/>
+                <ProductText kind="qty">1</ProductText>
+                <Add sx={{fontSize: "16px"}}/>
+              </PriceContainer>
+             </ProductItem>
+             <ProductItem><Close sx={{fontSize: "20px"}}/></ProductItem>
+          </Product> 
+          <Line/>
         </Info>
         <Summary>
           Summary
