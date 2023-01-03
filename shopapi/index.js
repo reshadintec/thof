45966947b0483f29fae6770b2156732e3f4ca32e
reshadintec <105705121+reshadintec@users.noreply.github.com>
2023-01-03 21:04:42 +0100

    authentication

--- shopapi/index.js
+++ shopapi/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require ("dotenv");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+
 dotenv.config();
 
 mongoose
@@ -14,6 +17,7 @@ mongoose
 
 app.use(express.json());
 app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
   console.log("Server Started Running at:" + process.env.PORT);
