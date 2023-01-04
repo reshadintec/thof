7f1694f51f4411c3b32813ab80f470902a9b6b79
reshadintec <105705121+reshadintec@users.noreply.github.com>
2023-01-04 13:56:59 +0100

    hashing pasword cryptojs

--- shopapi/routes/auth.js
+++ shopapi/routes/auth.js
@@ -22,7 +22,18 @@ try{
 
 router.post("/login", async (req,res)=>{
   try{
-    const user = await User.findOne({username: req.body.username})
+    const user = await User.findOne({username: req.body.username});
+    !user && res.status(401).json("Wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
+
+    const { password, ...others} = user._doc;
+
+    res.status(200).json(others);
+
   }catch(err){
     res.status(500).json(err)
   } 
