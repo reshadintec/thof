b42f2bde10acb74c47e14713318051eeeba20bf0
reshadintec <105705121+reshadintec@users.noreply.github.com>
2023-01-04 14:11:12 +0100

    user routes

--- shopapi/routes/auth.js
+++ shopapi/routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("..//models/User");
 const CryptoJS = require ("crypto-js");
+const jwt = require ("jsonwebtoken");
+
 
 //REGISTER
 router.post("/register",async (req,res)=>{
@@ -30,9 +32,17 @@ router.post("/login", async (req,res)=>{
 
     OriginalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
 
+    const accessToken = jwt.sign({
+      id:user.id, 
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SEC,
+    {expiresIn: "3d"}
+    );
+
     const { password, ...others} = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({...others, accessToken});
 
   }catch(err){
     res.status(500).json(err)
