3bf26023443c766897812bcb81e15aa8a69da76a
reshadintec <105705121+reshadintec@users.noreply.github.com>
2023-01-10 17:47:41 +0100

    getting user data

--- shopapi/routes/user.js
+++ shopapi/routes/user.js
@@ -1,5 +1,45 @@
+const User = require("../models/User");
+const { verifyToken, veriftyTokenAndAuthorization, veriftyTokenAndAdmin } = require("./veriftyToken");
+
 const router = require("express").Router();
 
-router.put("/:id",)
+//UPDATE
 
+router.put("/:id", veriftyTokenAndAuthorization,async (req,res)=>{
+  if(req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+  }
+  try{
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+      $set : req.body
+    },{new:true}
+    );
+    res.status(200).json(updatedUser);
+  }catch(err){res.status(500).json(err);
+  }
+});
+
+//DELETE 
+
+router.delete("/:id", veriftyTokenAndAuthorization, async(req,res)=>{
+  try{
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been successfully deleted")
+  }catch(err){
+    res.status(500).json(err)
+  }
+});
+
+
+//GET USER
+
+router.get("find/:id", veriftyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+   const  {password, ...others} = user._doc;
+   res.status(200).json(others);
+    }catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
