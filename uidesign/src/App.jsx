150685f236223757bef1cfd281fded0d9099f005
reshadintec <105705121+reshadintec@users.noreply.github.com>
2023-01-24 19:06:05 +0100

    scroll to top issue solved

--- uidesign/src/App.jsx
+++ uidesign/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, ScrollRestoration } from "react-router-dom";
 import { Shop } from './pages/Shop';
 import { SingleProduct } from './pages/SingleProduct';
 import { Register } from './pages/Register';
 import { SignIn } from './pages/SignIn';
 import { Cart } from './pages/Cart';
+import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <Router>
+      <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/shop/:category' element={<Shop/>} />
