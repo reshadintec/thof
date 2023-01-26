d1afaa78ee1128ff4ce6cabc14d4938d642e21a7
reshadintec <105705121+reshadintec@users.noreply.github.com>
2023-01-26 10:36:17 +0100

    login page for dashboard

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css"
+import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
@@ -9,23 +9,31 @@ import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import Product from "./pages/productSingle/Product";
 import ProductNew from "./pages/productNew/ProductNew";
+import Login from "./pages/login/Login";
+import { useSelector } from "react-redux";
 
 function App() {
+  const admin = useSelector((state) => state.user.currentUser.isAdmin);
   return (
-    <Router >
-      <Navbar/>
-      <div className="container">
-        <Sidebar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>          
-          <Route path="/users" element={<UserList/>}/>          
-          <Route path="/products" element={<Products/>}/>
-          <Route path="/product/:productId" element={<Product/>}/>
-          <Route path="/add-product" element={<ProductNew/>} />
-          <Route path="/user/:userId" element={<User/>}/>
-          <Route path="/newUser" element={<NewUser/>}/>
-        </Routes>
-      </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {admin && (
+          <>
+            <Navbar />
+            <div className="container">
+              <Sidebar />
+              <Route path="/" element={<Home />} />
+              <Route path="/users" element={<UserList />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/product/:productId" element={<Product />} />
+              <Route path="/add-product" element={<ProductNew />} />
+              <Route path="/user/:userId" element={<User />} />
+              <Route path="/newUser" element={<NewUser />} />
+            </div>
+          </>
+        )}
+      </Routes>
     </Router>
   );
 }
